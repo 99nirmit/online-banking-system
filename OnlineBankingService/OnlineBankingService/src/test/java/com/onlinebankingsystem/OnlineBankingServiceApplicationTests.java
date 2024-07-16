5d215431112b41efdcc75aab0a78b5fd456ea66f
99nirmit <79360867+99nirmit@users.noreply.github.com>
2024-07-17 01:34:40 +0530

    Initial commit with Spring Boot setup

---
+++ OnlineBankingService/OnlineBankingService/src/test/java/com/onlinebankingsystem/OnlineBankingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinebankingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBankingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
