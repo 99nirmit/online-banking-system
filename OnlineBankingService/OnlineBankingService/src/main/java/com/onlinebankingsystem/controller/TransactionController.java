b35db926c5c28298387f037bb9ae838f6c56885f
99nirmit <79360867+99nirmit@users.noreply.github.com>
2024-07-17 18:55:48 +0530

    changes in controller

--- OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/TransactionController.java
+++ OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/TransactionController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/acc")
@@ -17,6 +14,7 @@ public class TransactionController {
     @Autowired
     private TransactionalServiceImpl transactionalService;
 
+    @PostMapping("/transfer")
     public ResponseEntity<String> transfer(@RequestParam Long fromAccountId, @RequestParam Long toAccountId, double amount){
         try{
             transactionalService.transfer(fromAccountId, toAccountId,amount);
