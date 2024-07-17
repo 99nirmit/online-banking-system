f800b3a9f85ac656a33b117340deb12c40f9acec
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-07-17 16:44:01 +0530

    Created UserController with endpoints for user operations.

--- OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/UserController.java
+++ OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/UserController.java
@@ -1,24 +1,47 @@
 package com.onlinebankingsystem.controller;
 
-import com.onlinebankingsystem.dto.UserDTO;
+import com.onlinebankingsystem.dto.UserDto;
 import com.onlinebankingsystem.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @PostMapping("/register")
-    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
-        return userService.registerUser(userDTO);
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
+        UserDto userDto = userService.getUserById(id);
+        if (userDto != null) {
+            return ResponseEntity.ok(userDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
-    @PostMapping("/login")
-    public UserDTO loginUser(@RequestParam String username, @RequestParam String password) {
-        return userService.loginUser(username, password);
+    @PostMapping("/register")
+    public ResponseEntity<UserDto> registerUser(@RequestBody UserDto userDto) {
+        UserDto registeredUser = userService.registerUser(userDto);
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+        UserDto updatedUser = userService.updateUser(id, userDto);
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
