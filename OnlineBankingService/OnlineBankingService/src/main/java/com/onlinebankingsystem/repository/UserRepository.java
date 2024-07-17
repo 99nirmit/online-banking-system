d30dfeafef704b830fa40fbca37747a35ded79ff
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-07-17 15:03:09 +0530

    taking attributes of userEntity and UserDto and mapping UserEntity with Database

--- OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/repository/UserRepository.java
+++ OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/repository/UserRepository.java
@@ -1,7 +1,5 @@
 package com.onlinebankingsystem.repository;
 
-import com.onlinebankingsystem.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository {
 }
