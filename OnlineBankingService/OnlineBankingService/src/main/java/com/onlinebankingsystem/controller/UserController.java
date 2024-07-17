e92d7209f5b0c2b988d913df630b7d6b9ee7aeb7
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-07-17 15:28:15 +0530

    Add UserController with endpoints for user registration and login

--- OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/UserController.java
+++ OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.onlinebankingsystem.controller;
 
+import com.onlinebankingsystem.dto.UserDTO;
+import com.onlinebankingsystem.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public UserDTO registerUser(@RequestBody UserDTO userDTO) {
+        return userService.registerUser(userDTO);
+    }
+
+    @PostMapping("/login")
+    public UserDTO loginUser(@RequestParam String username, @RequestParam String password) {
+        return userService.loginUser(username, password);
+    }
 }
