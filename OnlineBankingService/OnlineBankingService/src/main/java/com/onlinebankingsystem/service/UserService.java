4f3cd4ae86414f1488aed63e2ac75d94baffbb08
Mohit Kulkarni <124132557+mohitkulkarni1999@users.noreply.github.com>
2024-07-17 15:22:05 +0530

    Define UserService interface with registerUser and loginUser methods

--- OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/service/UserService.java
+++ OnlineBankingService/OnlineBankingService/src/main/java/com/onlinebankingsystem/service/UserService.java
@@ -1,4 +1,8 @@
 package com.onlinebankingsystem.service;
 
+import com.onlinebankingsystem.dto.UserDTO;
+
 public interface UserService {
+    UserDTO registerUser(UserDTO userDTO);
+    UserDTO loginUser(String username, String password);
 }
